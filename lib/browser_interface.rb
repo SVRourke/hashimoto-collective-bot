27256b75376ad2e806415b3fabba872dee9835df
SVR <samuel@rourke.tech>
2020-08-17 16:57:11 -0400

    worked on browser_interface

--- lib/browser_interface.rb
+++ lib/browser_interface.rb
@@ -1,66 +1,83 @@
 class BrowserInterface
-    def initialize()
+
+
+    # Opens a browser on initialization
+    def initialize(account)
         @browser = Selenium::WebDriver.for :firefox
-    end
-    # returns a hash of the products details for use by the product object
-    def product_info(url)
-        info = Hash.new()
-        info[:url] = url
-        @browser.navigate.to url
-        info[:title] = @browser.find_element(:css, "div.sku-title > h1").text()
-        # get availability
-        avail_text = @browser.find_element(:css, "button.add-to-cart-button").text().upcase
-        
-        if avail_text != "ADD TO CART"
-            info[:availability] = false
-        else
-            info[:availability] = true
-        end
-
-        info
+        @account = account 
     end
 
-    def product_available?(product)
-        @browser.navigate.to product.url
-        sleep(3)
-        avail_text = @browser.find_element(:css, "button.add-to-cart-button").text().upcase
-        avail_text != "ADD TO CART" ? false : true
+    # =======================================================
+    # LOGS IN TO THE ACCOUNT, LEAVES BROWSER @ TARGET URL
+
+    def login
+        # get email field
+        email_field = @browser.find_element(:css, "input#login-email")
+        # get password field
+        password_field = @browser.find_element(:css, "input#login-password")
+        # get submit button
+        login_button = @browser.find_element(:css, "input#login-password")
+
+
     end
-
-
     
-    # logs the browser in to bestbuy
-    # leaves the browser on the home page after checking login status
-    def login(username, password)
-        @browser.navigate.to "https://www.bestbuy.com/identity/global/signin"
-        @browser.find_element(:xpath, '//*[@id="fld-e"]').send_keys(username)
-        @browser.find_element(:xpath, '//*[@id="fld-p1"]').send_keys(password)
-        @browser.find_element(:css, 'button.cia-form__controls__submit').click()
-        sleep(10)
-        # check to make sure login was successfull
-        icon = @browser.find_element(:xpath, "/html/body/div[2]/div/div/div/header/div[2]/div[2]/div/nav[2]/ul/li[1]/button/div[2]/span")
-
-        if icon.text != "Account"
-            puts "success"
-        else
-            puts "FATAL LOGIN ERROR"
-            self.down()
-        end
+    # =======================================================
+    # CHECKS PRODUCT AVAILABILITY
+    def in_stock?
+        buy_button = @browser.find_element(:css, "button.shopify-payment-button__button").text().upcase
+        buy_button != ""
     end
+    
+    # =======================================================
+    # ADD TO CART
 
-    # adds a product to the cart given a url
-    # leaves browser on cart page after
-    def addToCart(product)
-        @browser.navigate.to product
-        sleep(10)
-        puts driver.find_element(:css, "button.add-to-cart-button").click()
-        sleep(10)
-        driver.navigate.to "https://www.bestbuy.com/cart"
-        sleep(10)
+    def add_to_cart
+    end
+    
+    # =======================================================
+    # CHECKOUT
+    def checkout
+    end
+    
+    # =======================================================
+    # HELPERS
+
+    # navigates to the desired url
+    def goto_page(url)
+        @browser.navigate.to url
+    end
+    
+    # reload
+    def reload
+        @browser.navigate.refresh()
     end
     
     # ALWAYS RUN WHEN STOPPING PROGRAM
     def down
         @browser.quit
     end
+
+    # =======================================================
+    # =======================================================
+    # =======================================================
+
+    # def product_available?(product)
+    #     @browser.navigate.to product.url
+    #     sleep(3)
+    #     avail_text = @browser.find_element(:css, "button.add-to-cart-button").text().upcase
+    #     avail_text != "ADD TO CART" ? false : true
+    # end
+
+    # # adds a product to the cart given a url
+    # # leaves browser on cart page after
+    # def addToCart(product)
+    #     @browser.navigate.to product
+    #     sleep(10)
+    #     puts driver.find_element(:css, "button.add-to-cart-button").click()
+    #     sleep(10)
+    #     driver.navigate.to "https://www.bestbuy.com/cart"
+    #     sleep(10)
+    # end
+    
+    
 end

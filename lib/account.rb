9c0e6aface54f2472323e1566c1ef705f3f6062a
SVR <samuel@rourke.tech>
2020-08-18 17:59:51 -0400

    changed structure of the account info storage

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,6 @@
 class Account
-    attr_accessor :email, :password
+    attr_accessor :card_info, :info
+    # attr_accessor :email, :password,:email, :first_name, :last_name, :address_1, :apt_suite, :city, :country, :state, :zip, :card
     def initialize(attrs)
         attrs.each do |k, v|
             self.send("#{k}=", v)
